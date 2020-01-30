cbb871ebae58300ba9d9cbc355abccff0334243f
Orlando Vinícius <orlando.vinicius1987@gmail.com>
2020-01-30 01:30:22 +0000

    Acerto no logo

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -18,7 +18,7 @@
 <body>
 	<nav class="navbar navbar-expand-sm navbar-light">
 		<a class="navbar-brand" href="/">
-			<img src="images/feira_logo.jpg" height="90" title="Cestas Logo">
+			<img src="/images/feira_logo.jpg" height="90" title="Cestas Logo">
 		</a>
 		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 			<span class="navbar-toggler-icon"></span>
